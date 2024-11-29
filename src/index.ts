a788ea426cb8bd05dc2d23791d97448438f9eadb
Amit Dubey <iamsaagar@yahoo.com>
2024-11-29 21:45:00 +0000

    Updated

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(PORT);
+
 app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
